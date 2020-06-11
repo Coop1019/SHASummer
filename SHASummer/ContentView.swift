0d222e63e5125102842282d6e14676683a28857b
Cooper LeComp <cooper@cooperlecomp.com>
2020-06-11 12:46:59 -0700

    Initial Commit

---
+++ SHASummer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SHASummer
+//
+//  Created by Cooper LeComp on 6/11/20.
+//  Copyright © 2020 Cooper LeComp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
